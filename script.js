18fb9ce45c09144917b9e4a22b12ea2310c8400e
kunal052001 <kunalsadashivpatil@gmail.com>
2024-09-11 14:27:39 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,138 @@
+$(document).ready(function() {
+
+    //sticky header
+      $(window).scroll(function() {
+        if ($(this).scrollTop() > 1) {
+          $(".header-area").addClass("sticky");
+        } else {
+          $(".header-area").removeClass("sticky");
+        }
+    
+        // Update the active section in the header
+        updateActiveSection();
+      });
+    
+      $(".header ul li a").click(function(e) {
+        e.preventDefault(); 
+    
+        var target = $(this).attr("href");
+    
+        if ($(target).hasClass("active-section")) {
+          return; 
+        }
+    
+        if (target === "#home") {
+          $("html, body").animate(
+            {
+              scrollTop: 0 
+            },
+            500
+          );
+        } else {
+          var offset = $(target).offset().top - 40; 
+    
+          $("html, body").animate(
+            {
+              scrollTop: offset
+            },
+            500
+          );
+        }
+    
+        $(".header ul li a").removeClass("active");
+        $(this).addClass("active");
+      });
+    
+  
+      //Initial content revealing js
+      ScrollReveal({
+        distance: "100px",
+        duration: 2000,
+        delay: 200
+      });
+    
+      ScrollReveal().reveal(".header a, .profile-photo, .about-content, .education", {
+        origin: "left"
+      });
+      ScrollReveal().reveal(".header ul, .profile-text, .about-skills, .internship", {
+        origin: "right"
+      });
+      ScrollReveal().reveal(".project-title, .contact-title", {
+        origin: "top"
+      });
+      ScrollReveal().reveal(".projects, .contact", {
+        origin: "bottom"
+      });
+  
+    //contact form to excel sheet
+    const scriptURL = 'https://script.google.com/macros/s/AKfycbzUSaaX3XmlE5m9YLOHOBrRuCh2Ohv49N9bs4bew7xPd1qlgpvXtnudDs5Xhp3jF-Fx/exec';
+    const form = document.forms['submitToGoogleSheet']
+    const msg = document.getElementById("msg")
+  
+    form.addEventListener('submit', e => {
+        e.preventDefault()
+        fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+            .then(response => {
+                msg.innerHTML = "Message sent successfully"
+                setTimeout(function () {
+                    msg.innerHTML = ""
+                }, 5000)
+                form.reset()
+            })
+            .catch(error => console.error('Error!', error.message))
+    })
+      
+    });
+    
+    function updateActiveSection() {
+      var scrollPosition = $(window).scrollTop();
+    
+      // Checking if scroll position is at the top of the page
+      if (scrollPosition === 0) {
+        $(".header ul li a").removeClass("active");
+        $(".header ul li a[href='#home']").addClass("active");
+        return;
+      }
+    
+      // Iterate through each section and update the active class in the header
+      $("section").each(function() {
+        var target = $(this).attr("id");
+        var offset = $(this).offset().top;
+        var height = $(this).outerHeight();
+    
+        if (
+          scrollPosition >= offset - 40 &&
+          scrollPosition < offset + height - 40
+        ) {
+          $(".header ul li a").removeClass("active");
+          $(".header ul li a[href='#" + target + "']").addClass("active");
+        }
+      });
+    }
+    
+  
+   //curosal
+   const carousel = document.querySelector('.projects-carousel');
+const nextBtn = document.querySelector('.next-btn');
+const prevBtn = document.querySelector('.prev-btn');
+let scrollAmount = 0;
+
+nextBtn.addEventListener('click', () => {
+    const carouselWidth = document.querySelector('.carousel-wrapper').offsetWidth;
+    const projectWidth = document.querySelector('.project').offsetWidth;
+    const scrollMax = carousel.scrollWidth - carouselWidth;
+
+    scrollAmount += projectWidth;
+    if (scrollAmount > scrollMax) scrollAmount = scrollMax;
+
+    carousel.style.transform = `translateX(-${scrollAmount}px)`;
+});
+
+prevBtn.addEventListener('click', () => {
+    const projectWidth = document.querySelector('.project').offsetWidth;
+
+    scrollAmount -= projectWidth;
+    if (scrollAmount < 0) scrollAmount = 0;
+
+    carousel.style.transform = `translateX(-${scrollAmount}px)`;
+});
